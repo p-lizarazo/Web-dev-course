89869f9b0e3f589e9e9dd90335faaee635e90153
Pedro Santiago Lizarazo <p-lizarazo@javeriana.edu.co>
2020-10-07 14:32:57 -0500

    changing security

---
+++ back/src/main/java/co/edu/javeriana/pw/back/model/CommentRepository.java
@@ -0,0 +1,7 @@
+package co.edu.javeriana.pw.back.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CommentRepository extends CrudRepository<Comment,Long> {
+    
+}
--- back/src/main/java/co/edu/javeriana/pw/back/model/Forum.java
+++ back/src/main/java/co/edu/javeriana/pw/back/model/Forum.java
@@ -1,12 +1,17 @@
 package co.edu.javeriana.pw.back.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Forum {
+       
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     
-    Long id;
-
     
 
 }
