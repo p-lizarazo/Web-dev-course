821b673842a030318cf16607458dd90629b9328d
Pedro Santiago Lizarazo <p-lizarazo@javeriana.edu.co>
2020-10-12 19:57:18 -0500

    Votes created

--- back/src/main/java/co/edu/javeriana/pw/back/model/CommentVoteRepository.java
+++ back/src/main/java/co/edu/javeriana/pw/back/model/CommentVoteRepository.java
@@ -1,7 +1,21 @@
 package co.edu.javeriana.pw.back.model;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CommentVoteRepository extends CrudRepository<CommentVote,Long> {
-    
+    List<CommentVote> findByComentario(Comment comment);
+
+    @Query(value = "SELECT COUNT(*) FROM COMMENT_VOTE WHERE COMENTARIO_ID = ?1 AND ME_GUSTA = TRUE",
+    nativeQuery = true)
+    Long findCommentLikes(Long commentId);
+
+    @Query(value = "SELECT COUNT(*) FROM COMMENT_VOTE WHERE COMENTARIO_ID = ?1 AND ME_GUSTA = FALSE",
+    nativeQuery = true)
+    Long findCommentDislikes(Long commentId);
+
+    Optional<CommentVote> findByOwnerUsernameAndComentario(String ownerUsername, Comment comment);
 }
--- back/src/main/java/co/edu/javeriana/pw/back/model/TopicVoteRepository.java
+++ back/src/main/java/co/edu/javeriana/pw/back/model/TopicVoteRepository.java
@@ -1,7 +1,21 @@
 package co.edu.javeriana.pw.back.model;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TopicVoteRepository extends CrudRepository<TopicVote,Long> {
-    
+    List<TopicVote> findByTema(Topic topic);
+
+    @Query(value = "SELECT COUNT(*) FROM TOPIC_VOTE WHERE TEMA_ID = ?1 AND ME_GUSTA = TRUE",
+    nativeQuery = true)
+    Long findTopicsLikes(Long topicId);
+
+    @Query(value = "SELECT COUNT(*) FROM TOPIC_VOTE WHERE TEMA_ID = ?1 AND ME_GUSTA = FALSE",
+    nativeQuery = true)
+    Long findTopicsDislikes(Long topicId);
+
+    Optional<TopicVote> findByOwnerUsernameAndTema(String ownerUsername, Topic topic);
 }
